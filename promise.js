b2710774a66200f6c671deaa269826ed381461fe
Kevin Boyle <papershack@gmail.com>
2019-11-10 16:22:35 -0800

    add promise solution to async setup

---
+++ promise.js
@@ -0,0 +1,37 @@
+const posts = [
+    { title: 'Post One', body: 'Here is post 1' },
+    { title: 'Post Two', body: 'Here is post 2' }
+]
+
+function getPosts(){
+    setTimeout(() => {
+        let output = ''
+        posts.forEach((post, i) => {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output
+    }, 1000)
+}
+
+function createPost(post){
+    return new Promise((resolve, reject) => {      
+        setTimeout(() => {
+            posts.push(post)
+    
+            const error = false
+            // set error to true to see .catch() in action
+            
+            if (!error){
+                resolve()
+            } else {
+                reject('Error: Something went wrong!!')
+            }
+        }, 2000)
+    })
+}
+
+createPost({ title: 'Post Three', body: 'This is posts 3' })
+.then(getPosts)
+.catch(err => console.log(err))
+
+
