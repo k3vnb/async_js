63535bad08afcf04148373b74fc66f6d0c21a484
Kevin Boyle <papershack@gmail.com>
2019-11-10 16:46:03 -0800

    add async await solution

--- asyncAwait.js
+++ asyncAwait.js
@@ -37,18 +37,3 @@ async function init(){
    getPosts()
 }
 init()
-
-// createPost({ title: 'Post Three', body: 'This is posts 3' })
-// .then(getPosts)
-// .catch(err => console.log(err))
-
-// Promise.all
-// const promise1 = Promise.resolve('Hello World')
-// const promise2 = 10
-// const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'))
-// const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
-// .then(res => res.json())
-
-
-// Promise.all([promise1, promise2, promise3, promise4]).then((vals) => console.log(vals))
-
